81e47623f58d84a1f229e9ca971ba1ba51caa434
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-02-25 10:31:28 +0100

    Created the Xcode Project

---
+++ MorseBite/MorseBite/MorseBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseBiteApp.swift
+//  MorseBite
+//
+//  Created by Domenico Cafarelli on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MorseBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
